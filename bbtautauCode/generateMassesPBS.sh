82422f6b77e5aa288317d170e6b7419d16a7934b
Robin Aggleton <robin.aggleton@cern.ch>
2015-02-19 16:21:50 +0000

    Update mass range for pbs script

--- bbtautauCode/generateMassesPBS.sh
+++ bbtautauCode/generateMassesPBS.sh
@@ -2,7 +2,7 @@
 
 nevt=10000
 
-for i in {10..10}
+for i in {15..60}
 do
     echo "qsub -N bbtt$i -v args=\"-n ${nevt} --write --mass ${i} --seed ${i} --name /storage/ra12451/bbtautau/bbtautau_ma1_${i}_${nevt}.hepmc\" runMassBatchJobPBS.sh"
     qsub -N bbtt$i -v args="-n ${nevt} --write --mass ${i} --seed ${i} --name /storage/ra12451/bbtautau/bbtautau_ma1_${i}_${nevt}.hepmc" runMassBatchJobPBS.sh
