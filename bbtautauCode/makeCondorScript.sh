7af0df7c4ee2669943e9269280741e180cd055b2
Robin Aggleton <robin.aggleton@cern.ch>
2015-06-26 14:00:28 +0100

    Update condor script for new prog opts and low masses

--- bbtautauCode/makeCondorScript.sh
+++ bbtautauCode/makeCondorScript.sh
@@ -2,11 +2,12 @@
 
 # Make a condor script to run over user-defined masses, number of events, etc
 
-MASS_START=15
-MASS_END=60
-MASS_STEP=5
+MASS_START=4
+MASS_END=12
+MASS_STEP=4
 
-NEVT=100000
+NEVT=10000
+NJOBS=10
 
 newfile="generateMasses_${MASS_START}_${MASS_END}_${NEVT}.condor"
 
@@ -16,7 +17,7 @@ cp generateMasses.condor $newfile
 
 for (( m=$MASS_START; m <= $MASS_END; m = m + $MASS_STEP ));
 do
-    echo "arguments = -n ${NEVT} --write --mass ${m} --seed ${m} --name bbtautau_ma1_${m}_${NEVT}.hepmc" >> $newfile
-    echo "queue" >> $newfile
+    echo "arguments = -n ${NEVT} --hepmc --mass ${m} --seed \$(process) --tautau\n" >> $newfile
+    echo "queue $NJOBS" >> $newfile
 
 done
