d602b25632f2fd444cbc40319b6685adfd600830
Robin Aggleton <robin.aggleton@cern.ch>
2015-07-13 13:55:32 +0100

    Add Pythia and ROOT histogram output to pythia generation code.
    Easiest way to add ROOT is to use CMSSW (make sure it is ROOT5 and not 6 as it looks for CINT libs)
    Bit hacky, and require pythia to be compilied against the same root that CMSSW uses,
    along with the same Boost library.

--- bbtautauCode/bbtautau.cc
+++ bbtautauCode/bbtautau.cc
@@ -9,6 +9,11 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
+// ROOT headers
+#include "TH1.h"
+#include "TFile.h"
+
+// Own headers
 #include "Simul.cc"
 #include "PythiaProgramOpts.h"
 
@@ -73,6 +78,15 @@ int main(int argc, char *argv[]) {
 
   pythia.init();
 
+  // Any PYTHIA histograms here
+  Hist hTransverseMomentum("h transverse momentum", 40, 0, 200);
+  Hist a1Separation("a1 separation deltaR", 100, 0, 5);
+  Hist a1PhiSeparation("a1 separation deltaPhi", 31, 0, 3.1);
+  Hist a1Momentum("a1 momentum", 80, 0, 400);
+  Hist a1TransverseMomentum("a1 transverse momentum", 80, 0, 400);
+  Hist bbDR("a1 product separation deltaR", 100, 0, 5);
+  Hist bbDPhi("a1 product separation deltaPhi", 31, 0, 3.1);
+
   // Interface for conversion from Pythia8::Event to HepMC event.
   HepMC::Pythia8ToHepMC ToHepMC;
   HepMC::IO_GenEvent ascii_io(opts.filenameHEPMC(), std::ios::out);
@@ -100,6 +114,14 @@ int main(int argc, char *argv[]) {
   std::string stem = opts.filenameHEPMC().substr(0, opts.filenameHEPMC().size() - ext.size());
   progressFile.open(stem + "_progress.txt");
 
+  //////////////////////
+  // SETUP ROOT STUFF //
+  //////////////////////
+  TFile* outFile = new TFile("hist.root", "RECREATE");
+
+  TH1F * h_bbDr = new TH1F("bbDr","bb DeltaR", 100, 0, 5);
+
+
   //////////////////////////
   // GENERATE EVENTS HERE //
   //////////////////////////
@@ -124,6 +146,39 @@ int main(int argc, char *argv[]) {
       pythia.process.list();
     }
 
+    // find h decay products and look at separation
+    for (int i = 0; i < pythia.event.size(); ++i) {
+
+      if (abs(pythia.event[i].id()) == 25 && pythia.event[i].status() == -62) {
+
+        // plot h1 daughter variables
+        int d1 = pythia.event[i].daughter1();
+        int d2 = pythia.event[i].daughter2();
+        a1Separation.fill(REtaPhi(pythia.event[d1].p(), pythia.event[d2].p()));
+        a1PhiSeparation.fill(phi(pythia.event[d1].p(), pythia.event[d2].p()));
+
+        // figure out which is the a1
+        int a1Ind = d1;
+        if (pythia.event[d1].id() != 36) {
+          a1Ind = d2;
+        }
+        // plot h1 variables
+        hTransverseMomentum.fill(pythia.event[i].pT());
+
+        // now plot a1 variables, and for its decay products
+        a1Momentum.fill(pythia.event[a1Ind].pAbs());
+        a1TransverseMomentum.fill(pythia.event[a1Ind].pT());
+
+        // look at a1 daughter particles
+        Vec4 daughter1Mom = pythia.event[pythia.event[a1Ind].daughter1()].p();
+        Vec4 daughter2Mom = pythia.event[pythia.event[a1Ind].daughter2()].p();
+        bbDR.fill(REtaPhi(daughter1Mom, daughter2Mom));
+        h_bbDr->Fill(REtaPhi(daughter1Mom, daughter2Mom));
+        bbDPhi.fill(phi(daughter1Mom, daughter2Mom));
+
+      }
+    }
+
     // Construct new empty HepMC event and fill it.
     // Write the HepMC event to file. Done with it.
     if (opts.writeToHEPMC()) {
@@ -146,10 +201,23 @@ int main(int argc, char *argv[]) {
   progressFile.close();
   pythia.stat();
 
+  cout << hTransverseMomentum << endl;
+  cout << a1PhiSeparation << endl;
+  cout << a1Separation << endl;
+  cout << a1Momentum << endl;
+  cout << a1TransverseMomentum << endl;
+  cout << bbDR << endl;
+  cout << bbDPhi << endl;
+
   if (opts.writeToLHE()) {
     // Update the cross section info based on Monte Carlo integration during run.
     myLHA->updateSigma();
     // Write endtag. Overwrite initialization info with new cross sections.
     myLHA->closeLHEF(true);
   }
+
+  h_bbDr->Write();
+  outFile->Close();
+  delete outFile;
+
 }
