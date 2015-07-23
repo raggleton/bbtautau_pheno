eb4d878171d89137f44471ec4ebc5c367248bb66
Robin Aggleton <robin.aggleton@cern.ch>
2015-07-23 11:15:09 +0100

    Add in script and plots to compare deltaR(bb)

---
+++ bbtautauCode/za1_hists/compare_dr.py
@@ -0,0 +1,56 @@
+"""
+Simple scirpt to plot DeltaR distributions from files
+"""
+import ROOT
+from collections import OrderedDict
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gStyle.SetOptStat(0)
+ROOT.gROOT.SetBatch(1)
+
+files = OrderedDict()
+files["hist_za1_20.root"] = dict(color=ROOT.kRed, linestyle=1)
+files["hist_za1_30.root"] = dict(color=ROOT.kBlue, linestyle=1)
+files["hist_za1_40.root"] = dict(color=ROOT.kMagenta, linestyle=1)
+files["hist_za1_50.root"] = dict(color=ROOT.kGreen+2, linestyle=1)
+files["hist_za1_60.root"] = dict(color=ROOT.kOrange, linestyle=1)
+files["hist_za1_70.root"] = dict(color=ROOT.kViolet+7, linestyle=1)
+files["hist_za1_80.root"] = dict(color=ROOT.kBlack, linestyle=1)
+
+stack = ROOT.THStack("stack","h(450) -> Za1, a1 -> bb");
+leg = ROOT.TLegend(0.6, 0.55, 0.88, 0.88)
+leg.SetLineWidth(0)
+leg.SetLineColor(0)
+
+for i, (fname, settings) in enumerate(files.items()):
+    mass = fname.replace("hist_za1_", "").replace(".root", "")
+
+    f = ROOT.TFile(fname)
+    if not f:
+        print "cannot open", fname
+        continue
+
+    h = f.Get("bbDr").Clone("h"+mass)
+    if not h:
+        print "cannot get hist"
+        continue
+
+    h.SetLineColor(settings['color'])
+    h.SetFillColorAlpha(settings['color'], 0.55)
+    h.SetLineStyle(settings['linestyle'])
+    h.Scale(1./h.Integral())
+    h.SetAxisRange(0, 1.5, 'X')
+    h.SetDirectory(0)  # VERY IMPORTANT otherwise TFile owns hist
+    stack.Add(h)
+    leg.AddEntry(h, "m_{a1} = %s GeV " % mass, "LF")
+    f.Close()
+
+
+c = ROOT.TCanvas("c", "", 800, 800)
+c.SetTicks(1,1)
+stack.Draw("NOSTACK")
+leg.Draw()
+master = stack.GetHistogram()
+master.SetAxisRange(0, 1.5, 'X')
+master.SetTitle("h(450) -> Za1, a1 -> b;#Delta R(bb);p.d.f.")
+c.SaveAs("za1_dr.pdf")
